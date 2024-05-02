f3f89c93471d23d9d2596e557719616afea56cad
Maria <Usuario@CPD02-M18>
2024-05-02 15:50:07 -0300

    Criacao - modificar mainpage

--- AppPedagio2/MainPage.xaml.cs
+++ AppPedagio2/MainPage.xaml.cs
@@ -2,23 +2,40 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+
+        private void ToolbarItem_Clicked_Somar(object sender, EventArgs e)
         {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void ToolbarItem_Clicked_Add(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ref_carregando_Refreshing(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lst_Pedagio_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
         }
     }
 
